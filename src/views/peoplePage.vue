f8fb7378c1ac8a1298554b5317e17b21674ad593
IKEA <jellynina@gmail.com>
2016-08-13 18:02:07 +0800

    People html

--- src/views/peoplePage.vue
+++ src/views/peoplePage.vue
@@ -9,6 +9,21 @@
         Tab Control
       </div>
     </div>
+    <div class="people-dash">
+      <div class="people-side">
+        <img src="../assets/logo.png" alt="iron-man" / class="host-img">
+        <h2>Name</h2>
+        <ul>
+          <li>chenhsu@mail.ncku.edu.tw </li>
+          <li>06-2757575 ext 54324</li>
+        </ul>
+        <h3>Resaerch Area</h3>
+      </div>
+      <div class="people-main">
+        <h2>Introduction</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni modi doloremque excepturi laudantium maxime explicabo cumque deleniti voluptate deserunt.</p>
+      </div>
+    </div>
 
 
   </div>
